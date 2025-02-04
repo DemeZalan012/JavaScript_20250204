8607c0657f0b3ca45917e5f3a7d4ff2ce7207a04
Nitro_zeus <deme.zalan.janos@diak.szbi-pg.hu>
2025-02-04 09:38:37 +0100

    sok számolás

---
+++ 01_valtozok.js
@@ -0,0 +1,16 @@
+var a = 2;
+console.log(a);
+
+let b = 3;
+b = 4;
+console.log(b)
+const c = 5;
+console.log(c);
+
+var x = 5;
+console.log(x);
+x = 6;
+console.log(x);
+
+let d = 3;
+console.log(d);
